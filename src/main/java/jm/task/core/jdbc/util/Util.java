1eaa39ebd9371e933186f44a262d38f712fc2609
Strelkatlet <ignatievwa@gmail.com>
2022-07-21 12:55:05 +0300

    задача 1.1.3 с исправлениями

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private  static final String PASSWORD = "root";
-    private  static final String USERNAME = "root";
-    private  static final String URL = "jdbc:mysql://localhost:3306/db";
+    private static final String PASSWORD = "root";
+    private static final String USERNAME = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/db";
 
     public static Connection getConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
